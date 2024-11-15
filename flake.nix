a2b201444c7af6a4b533b2a9f6efb53f6b3eab78
Matias Zwinger <matias.zwinger@aalto.fi>
2024-11-15 15:22:04 +0200

    feat: switch from crate2nix to crane

--- flake.nix
+++ flake.nix
@@ -2,86 +2,68 @@
   description = "Niksi";
 
   inputs = {
-    nixkpgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix.url = "github:nix-community/crate2nix";
-
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = [
-      "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    ];
-    extra-substituters = [
-      "https://eigenvalue.cachix.org"
-      "https://devenv.cachix.org"
-    ];
-    allow-import-from-derivation = true;
-  };
-
-  outputs = inputs @ {
+  outputs = {
+    self,
     nixpkgs,
-    flake-parts,
-    crate2nix,
+    crane,
+    flake-utils,
+    rust-overlay,
     ...
   }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-      ];
-
-      imports = [
-        inputs.devenv.flakeModule
-        ./nix/rust-overlay
-      ];
-
-      perSystem = {
-        system,
-        pkgs,
-        lib,
-        inputs',
-        ...
-      }: let
-        cargoNix = inputs.crate2nix.tools.${system}.appliedCargoNix {
-          name = "niksi";
-          src = ./.;
-        };
-      in rec {
-        imports = [
-          ./nix/devenv
-        ];
-
-        checks = {
-          lib = cargoNix.workspaceMembers.lib.build.override {
-            runTests = true;
-          };
-          cli = cargoNix.workspaceMembers.cli.build.override {
-            runTests = true;
-          };
-        };
-
-        packages = {
-          niksi = cargoNix.workspaceMembers.cli.build;
-          default = packages.niksi;
-
-          inherit (pkgs) rust-toolchain;
-
-          rust-toolchain-versions = pkgs.writeScriptBin "rust-toolchain-versions" ''
-            ${pkgs.rust-toolchain}/bin/cargo --version
-            ${pkgs.rust-toolchain}/bin/rustc --version
-          '';
-        };
-
-        apps = {
-          niksi = {
-            type = "app";
-            program = "${packages.niksi}/bin/niksi";
-          };
-          default = apps.niksi;
-        };
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
       };
-    };
+
+      inherit (pkgs) lib;
+
+      craneLib = crane.mkLib pkgs;
+
+      src = ./.;
+
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        buildInputs = lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      niksi-cli = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+        });
+
+      niksi-lib = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+        });
+    in {
+      checks = {
+        inherit niksi-cli niksi-lib;
+      };
+
+      packages.default = niksi-cli;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = niksi-cli;
+      };
+
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
+      };
+    });
 }
