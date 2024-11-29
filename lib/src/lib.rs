116fb71a85f056aca2ca4c7474fe6f6101929203
Luukas Pörtfors <luukas.portfors@aalto.fi>
2024-11-29 15:15:10 +0200

    feat(lib): specify name and version of the docker image

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,7 @@
 mod devcontainer;
 
+use anyhow::{Context, Result};
+
 use serde::Deserialize;
 use std::{
     path::PathBuf,
@@ -14,7 +16,7 @@ use devcontainer::DevContainer;
 /// The Schema of the niksi.json configuration file used for configuring a Dev Container
 #[derive(Deserialize, Debug, Clone)]
 pub struct NiksiConfig {
-    /// The name of the course or project
+    /// The name of the produced docker image
     pub name: String,
     /// Optional course code
     pub course_code: Option<String>,
@@ -39,6 +41,18 @@ pub struct Niksi {
     output_directory: PathBuf,
 }
 
+/// The Builder helper for the [`Niksi`] struct
+///
+/// Example
+/// ```rust
+/// let niksi = Niksi::builder()
+///     .config_file("niksi.json")
+///     .output_directory("./outdir")
+///     .lock_file("./outdir/niksi.lock")
+///     .build()?;
+///
+/// niksi.build();
+/// ```
 #[derive(Default, Debug, Clone)]
 pub struct NiksiBuilder {
     config_file: Option<PathBuf>,
@@ -47,25 +61,45 @@ pub struct NiksiBuilder {
 }
 
 impl Niksi {
+    /// Constructs a new [`NiksiBuilder`].
+    /// See the builders documentation for more information.
     pub fn builder() -> NiksiBuilder {
         NiksiBuilder::new()
     }
 
+    /// Outputs the JSON for .devcontainer.json as a String.
+    /// Fails if Serialization fails.
     pub fn devcontainer_json(&self) -> Result<String, serde_json::Error> {
         serde_json::to_string_pretty(&DevContainer::from(self.config.clone()))
     }
 
-    pub fn packages_nix(&self) -> String {
+    /// Outputs the overrides.nix file with paths, name and version set according to the
+    /// [`NiksiConfig`] provided.
+    pub fn overrides_nix(&self) -> String {
         format!(
-            "{{pkgs, ...}}:{{paths = with pkgs; [{}];}}",
-            self.config.packages.join(" ")
+            r#"{{pkgs, ...}}: {{
+                paths = with pkgs; [{}];
+                name = "{}";
+                tag = "{}";
+            }}"#,
+            self.config.packages.join(" "),
+            &self.config.name,
+            &self.config.version,
         )
     }
 
-    pub fn build(&self) -> Result<PathBuf, Box<dyn std::error::Error + 'static>> {
+    /// Builds the docker image as specified in the [`NiksiConfig`] by:
+    /// 1. Downloading the template or a default template
+    /// 2. Writing the result of [`Niksi::overrides_nix`] to overrides.nix
+    /// 3. Copying the niksi.lock file over, if one exists
+    /// 4. Running `nix build`
+    /// 5. Returning the path of the build result (in the Nix store)
+    ///
+    /// Fails if any of the Nix commands or fs access fails
+    pub fn build(&self) -> Result<PathBuf> {
         const DEFAULT_TEMPLATE: &str = "github:niksi-aalto/templates#plain";
 
-        let workdir = tempfile::tempdir()?;
+        let workdir = tempfile::tempdir().context("Failed to create tempdir to work in")?;
 
         Command::new("nix")
             .args([
@@ -83,17 +117,19 @@ impl Niksi {
                     .unwrap_or(DEFAULT_TEMPLATE.to_string()),
             )
             .current_dir(workdir.path())
-            .status()?;
+            .status()
+            .context("`nix flake init` failed with the specified template")?;
 
-        let extra_paths = format!(
-            "{{pkgs, ...}}:with pkgs; [{}]",
-            self.config.packages.join(" ")
-        );
-        let mut extras_file = std::fs::File::create(workdir.path().join("paths.nix"))?;
-        extras_file.write_all(extra_paths.as_bytes())?;
+        let overrides = self.overrides_nix();
+        let mut overrides_file = std::fs::File::create(workdir.path().join("overrides.nix"))
+            .context("Failed to create overrides.nix file")?;
+        overrides_file
+            .write_all(overrides.as_bytes())
+            .context("Failed to write overrides to overrides.nix")?;
 
         if self.lock_file.is_file() {
-            std::fs::copy(self.lock_file.as_path(), workdir.path().join("flake.lock"))?;
+            std::fs::copy(self.lock_file.as_path(), workdir.path().join("flake.lock"))
+                .context("Failed to copy niksi lockfile")?;
         }
 
         let result = Command::new("nix")
@@ -106,7 +142,8 @@ impl Niksi {
             ])
             .current_dir(workdir.path())
             .stderr(Stdio::inherit())
-            .output()?;
+            .output()
+            .context("Nix build failed")?;
 
         std::fs::copy(
             workdir.path().join("flake.lock"),
@@ -130,25 +167,36 @@ pub enum BuilderError {
 }
 
 impl NiksiBuilder {
+    /// Constructs a new [`NiksiBuilder`] similarly to [`Niksi::builder`]
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Specify the niksi json config file.
+    /// For the schema of the config file see [`NiksiConfig`]
+    ///
+    /// The config file is *required*, meaning that [`Niksibuilder::build`] will fail if no config
+    /// file has been specified.
     pub fn config_file(mut self, file: impl Into<PathBuf>) -> Self {
         self.config_file = Some(file.into());
         self
     }
 
+    /// Specify the output directory, if no directory is specified, the current directory is used.
     pub fn output_directory(mut self, dir: impl Into<PathBuf>) -> Self {
         self.output_directory = Some(dir.into());
         self
     }
 
+    /// Specify the lock file. If no file is specified a new lock file is generated upon running [`Niksi::build`].
     pub fn lock_file(mut self, file: impl Into<PathBuf>) -> Self {
         self.lock_file = Some(file.into());
         self
     }
 
+    /// Construct a new [`Niksi`] struct.
+    ///
+    /// fails if config file specification is invalid or missing.
     pub fn build(self) -> Result<Niksi, BuilderError> {
         let config = match self.config_file {
             Some(p) => {
