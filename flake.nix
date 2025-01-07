5d972df5f2d15be1bee47fa4c4eb2a5a3acaa048
Matias Zwinger <matias.zwinger@aalto.fi>
2025-01-07 18:39:50 +0200

    fix(flake): remove unnecessary env vars

--- flake.nix
+++ flake.nix
@@ -81,24 +81,11 @@
             config,
             ...
           }: {
-            packages = with pkgs; [skopeo];
+            packages = [pkgs.skopeo niksi];
 
             languages.rust = {
               enable = true;
               channel = "stable";
-            };
-
-            env = {
-              LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
-              BINDGEN_EXTRA_CLANG_ARGS =
-                (builtins.map (a: ''-I"${a}/include"'') [
-                  pkgs.glibc.dev
-                ])
-                ++ [
-                  ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-                  ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-                  ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-                ];
             };
           })
         ];
